7c081defa570cc6ab942956d203941d2543b1177
Oegerli, Maurice <maurice.oegerli@unisg.ch>
2019-10-07 16:56:56 +0200

    check if user exists

--- api/register.php
+++ api/register.php
@@ -1,69 +1,89 @@
 <?php
 include_once './config/database.php';
 
-header("Access-Control-Allow-Origin: * ");
+header("Access-Control-Allow-Origin: http://localhost:8081 ");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Access-Control-Allow-Origin, Authorization, X-Requested-With, Origin");
 
-$username = '';
-$password = '';
-$conn = null;
+if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {
+  http_response_code(204);
+} else {
 
-$databaseService = new DatabaseService();
-$conn = $databaseService->getConnection();
+  $username = '';
+  $password = '';
+  $conn = null;
 
-$data = json_decode(file_get_contents("php://input"));
+  $databaseService = new DatabaseService();
+  $conn = $databaseService->getConnection();
 
-if (isset($data->username) && isset($data->password)) {
-  $username = $data->username;
-  $password = $data->password;
+  $data = json_decode(file_get_contents("php://input"));
 
-  if (strlen($username) > 0 && strlen($password) > 0) {
+  if (isset($data->username) && isset($data->password)) {
+    $username = $data->username;
+    $password = $data->password;
 
-    $table_name = 'users';
+    if (strlen($username) > 0 && strlen($password) > 0) {
 
-    $query = "INSERT INTO " . $table_name . "
-                SET username = :username,
-                    password = :password";
+      $table_name = 'users';
 
-    $stmt = $conn->prepare($query);
+      $usernameQuery = "SELECT id, username FROM " . $table_name . " WHERE username = ? LIMIT 0,1";
 
-    $stmt->bindParam(':username', $username);
+      $stmt = $conn->prepare($usernameQuery);
+      $stmt->bindParam(1, $username);
+      $stmt->execute();
+      $num = $stmt->rowCount();
 
-    $password_hash = password_hash($password, PASSWORD_BCRYPT);
+      if ($num <= 0) {
+        $query = "INSERT INTO " . $table_name . "
+      SET username = :username,
+          password = :password";
 
-    $stmt->bindParam(':password', $password_hash);
+        $stmt = $conn->prepare($query);
 
+        $stmt->bindParam(':username', $username);
 
-    if ($stmt->execute()) {
-      http_response_code(200);
+        $password_hash = password_hash($password, PASSWORD_BCRYPT);
 
-      echo json_encode(array(
-        "message" => "Registration successful",
-        "username" => $username
-      ));
+        $stmt->bindParam(':password', $password_hash);
+
+        if ($stmt->execute()) {
+          http_response_code(200);
+
+          echo json_encode(array(
+            "message" => "Registration successful",
+            "username" => $username
+          ));
+        } else {
+          http_response_code(400);
+
+          echo json_encode(array("message" => "Registration failed"));
+        }
+      } else {
+        http_response_code(409);
+
+        echo json_encode(array(
+          "message" => "Username $username already taken",
+          "username" => $username
+        ));
+      }
+    } else if (strlen($username) <= 0 && strlen($password) > 0) {
+      http_response_code(400);
+
+      echo json_encode(array("message" => "Username can't be empty"));
+    } else if (strlen($username) > 0 && strlen($password) <= 0) {
+      http_response_code(400);
+
+      echo json_encode(array("message" => "Password can't be empty"));
     } else {
       http_response_code(400);
 
-      echo json_encode(array("message" => "Registration failed"));
+      echo json_encode(array("message" => "Username or password can't be empty"));
     }
-  } else if (strlen($username) <= 0 && strlen($password) > 0) {
-    http_response_code(400);
-
-    echo json_encode(array("message" => "Username can't be empty"));
-  } else if (strlen($username) > 0 && strlen($password) <= 0) {
-    http_response_code(400);
-
-    echo json_encode(array("message" => "Password can't be empty"));
   } else {
     http_response_code(400);
 
-    echo json_encode(array("message" => "Username or password can't be empty"));
+    echo json_encode(array("message" => "Username or password missing"));
   }
-} else {
-  http_response_code(400);
-
-  echo json_encode(array("message" => "Username or password missing"));
 }
