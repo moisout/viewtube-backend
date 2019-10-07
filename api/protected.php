da922a4c95ce8d306af6f29ded5bb0971dca670d
Oegerli, Maurice <maurice.oegerli@unisg.ch>
2019-10-07 16:58:17 +0200

    update cors

--- api/protected.php
+++ api/protected.php
@@ -5,7 +5,7 @@ require "../vendor/autoload.php";
 
 use \Firebase\JWT\JWT;
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: viewtube.eu");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
--- api/register.php
+++ api/register.php
@@ -1,7 +1,7 @@
 <?php
 include_once './config/database.php';
 
-header("Access-Control-Allow-Origin: http://localhost:8081 ");
+header("Access-Control-Allow-Origin: viewtube.eu");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Max-Age: 3600");
