68974226678baa438c274e014c68b0d068b82ea7
Oegerli, Maurice <maurice.oegerli@unisg.ch>
2019-10-08 17:56:15 +0200

    include captcha

--- api/register.php
+++ api/register.php
@@ -11,79 +11,116 @@ if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {
   http_response_code(204);
 } else {
 
-  $username = '';
-  $password = '';
-  $conn = null;
-
-  $databaseService = new DatabaseService();
-  $conn = $databaseService->getConnection();
-
   $data = json_decode(file_get_contents("php://input"));
 
-  if (isset($data->username) && isset($data->password)) {
-    $username = $data->username;
-    $password = $data->password;
+  // Captcha verification
 
-    if (strlen($username) > 0 && strlen($password) > 0) {
+  $url = 'https://captcheck.netsyms.com/api.php';
+  $captcheckData = [
+    'session_id' => $data->captcheck_session_code,
+    'answer_id' => $data->captcheck_selected_answer,
+    'action' => "verify"
+  ];
 
-      $table_name = 'users';
+  $options = [
+    'http' => [
+      'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+      'method' => 'POST',
+      'content' => http_build_query($captcheckData)
+    ]
+  ];
 
-      $usernameQuery = "SELECT id, username FROM " . $table_name . " WHERE username = ? LIMIT 0,1";
+  $context = stream_context_create($options);
+  $result = file_get_contents($url, false, $context);
+  $resp = json_decode($result, TRUE);
 
-      $stmt = $conn->prepare($usernameQuery);
-      $stmt->bindParam(1, $username);
-      $stmt->execute();
-      $num = $stmt->rowCount();
+  if (!$resp['result']) {
+    http_response_code(403);
 
-      if ($num <= 0) {
-        $query = "INSERT INTO " . $table_name . "
+    echo json_encode(array(
+      "message" => "CAPTCHA not verified: " . $resp['msg']
+    ));
+  } else {
+    // Registration
+
+    $username = '';
+    $password = '';
+    $conn = null;
+
+    $pw_min_length = 6;
+
+    $databaseService = new DatabaseService();
+    $conn = $databaseService->getConnection();
+
+    if (isset($data->username) && isset($data->password)) {
+      $username = $data->username;
+      $password = $data->password;
+
+      if (strlen($username) > 0 && strlen($password) >= $pw_min_length) {
+
+        $table_name = 'users';
+
+        $usernameQuery = "SELECT id, username FROM " . $table_name . " WHERE username = ? LIMIT 0,1";
+
+        $stmt = $conn->prepare($usernameQuery);
+        $stmt->bindParam(1, $username);
+        $stmt->execute();
+        $num = $stmt->rowCount();
+
+        if ($num <= 0) {
+          $query = "INSERT INTO " . $table_name . "
       SET username = :username,
           password = :password";
 
-        $stmt = $conn->prepare($query);
+          $stmt = $conn->prepare($query);
 
-        $stmt->bindParam(':username', $username);
+          $stmt->bindParam(':username', $username);
 
-        $password_hash = password_hash($password, PASSWORD_BCRYPT);
+          $password_hash = password_hash($password, PASSWORD_BCRYPT);
 
-        $stmt->bindParam(':password', $password_hash);
+          $stmt->bindParam(':password', $password_hash);
 
-        if ($stmt->execute()) {
-          http_response_code(200);
+          if ($stmt->execute()) {
+            http_response_code(200);
+
+            echo json_encode(array(
+              "message" => "Registration successful",
+              "username" => $username
+            ));
+          } else {
+            http_response_code(400);
+
+            echo json_encode(array("message" => "Registration failed"));
+          }
+        } else {
+          http_response_code(409);
 
           echo json_encode(array(
-            "message" => "Registration successful",
+            "message" => "Username \"$username\" already taken",
             "username" => $username
           ));
-        } else {
-          http_response_code(400);
-
-          echo json_encode(array("message" => "Registration failed"));
         }
+      } else if (strlen($username) <= 0 && strlen($password) > 0) {
+        http_response_code(400);
+
+        echo json_encode(array("message" => "Username can't be empty"));
+      } else if (strlen($username) > 0 && strlen($password) <= 0) {
+        http_response_code(400);
+
+        echo json_encode(array("message" => "Password can't be empty"));
+      } else if (strlen($username) > 0 && strlen($password) < $pw_min_length) {
+        http_response_code(400);
+
+        echo json_encode(array("message" => "Password is too short - at least $pw_min_length characters"));
       } else {
-        http_response_code(409);
+        http_response_code(400);
 
-        echo json_encode(array(
-          "message" => "Username $username already taken",
-          "username" => $username
-        ));
+        echo json_encode(array("message" => "Username or password can't be empty"));
       }
-    } else if (strlen($username) <= 0 && strlen($password) > 0) {
-      http_response_code(400);
-
-      echo json_encode(array("message" => "Username can't be empty"));
-    } else if (strlen($username) > 0 && strlen($password) <= 0) {
-      http_response_code(400);
-
-      echo json_encode(array("message" => "Password can't be empty"));
     } else {
       http_response_code(400);
 
-      echo json_encode(array("message" => "Username or password can't be empty"));
+      echo json_encode(array("message" => "Username or password missing"));
     }
-  } else {
-    http_response_code(400);
-
-    echo json_encode(array("message" => "Username or password missing"));
   }
 }
