b3fd43098207f04f0ded5d38a798629d547728c9
Maurice Oegerli <maurice@oeger.li>
2019-11-07 23:34:20 +0100

    work on video api

--- api/utils/mapper.php
+++ api/utils/mapper.php
@@ -9,7 +9,127 @@ class Mapper
   }
 
   public function mapVideoData($data)
-  { }
+  {
+    $mappedData = array();
+
+    $videoDetails = $data['args']['player_response']['videoDetails'];
+    $additionalDetails = $data['args']['player_response']['microformat']['playerMicroformatRenderer'];
+
+    $mappedData = array_merge($mappedData, [
+      'title' => $videoDetails['title'],
+      'videoId' => $videoDetails['videoId'],
+      'videoThumbnails' =>
+      array(
+        0 =>
+        array(
+          'quality' => 'String',
+          'url' => 'String',
+          'width' => 'Int32',
+          'height' => 'Int32',
+        ),
+      ),
+      'description' => $videoDetails['shortDescription'],
+      'descriptionHtml' => '[redacted]',
+      'published' => date_create((string)$additionalDetails['publishDate']),
+      'publishedText' => $this->time_elapsed_string($additionalDetails['publishDate']),
+      'keywords' => $videoDetails['keywords'],
+      'viewCount' => $videoDetails['viewCount'],
+      'likeCount' => 'Int32',
+      'dislikeCount' => 'Int32',
+      'paid' => 'Bool',
+      'premium' => 'Bool',
+      'isFamilyFriendly' => $additionalDetails['isFamilySafe'],
+      'allowedRegions' => $additionalDetails['availableCountries'],
+      'genre' => 'String',
+      'genreUrl' => 'String',
+      'author' => 'String',
+      'authorId' => 'String',
+      'authorUrl' => 'String',
+      'authorThumbnails' =>
+      array(
+        0 =>
+        array(
+          'url' => 'String',
+          'width' => 'Int32',
+          'height' => 'Int32',
+        ),
+      ),
+      'subCountText' => 'String',
+      'lengthSeconds' => 'Int32',
+      'allowRatings' => 'Bool',
+      'rating' => 'Float32',
+      'isListed' => 'Bool',
+      'liveNow' => 'Bool',
+      'isUpcoming' => 'Bool',
+      'premiereTimestamp' => 'Int64?',
+      'hlsUrl' => 'String?',
+      'adaptiveFormats' =>
+      array(
+        0 =>
+        array(
+          'index' => 'String',
+          'bitrate' => 'String',
+          'init' => 'String',
+          'url' => 'String',
+          'itag' => 'String',
+          'type' => 'String',
+          'clen' => 'String',
+          'lmt' => 'String',
+          'projectionType' => 'Int32',
+          'container' => 'String',
+          'encoding' => 'String',
+          'qualityLabel' => 'String?',
+          'resolution' => 'String?',
+        ),
+      ),
+      'formatStreams' =>
+      array(
+        0 =>
+        array(
+          'url' => 'String',
+          'itag' => 'String',
+          'type' => 'String',
+          'quality' => 'String',
+          'container' => 'String',
+          'encoding' => 'String',
+          'qualityLabel' => 'String',
+          'resolution' => 'String',
+          'size' => 'String',
+        ),
+      ),
+      'captions' =>
+      array(
+        0 =>
+        array(
+          'label' => 'String',
+          'languageCode' => 'String',
+          'url' => 'String',
+        ),
+      ),
+      'recommendedVideos' =>
+      array(
+        0 =>
+        array(
+          'videoId' => 'String',
+          'title' => 'String',
+          'videoThumbnails' =>
+          array(
+            0 =>
+            array(
+              'quality' => 'String',
+              'url' => 'String',
+              'width' => 'Int32',
+              'height' => 'Int32',
+            ),
+          ),
+          'author' => 'String',
+          'lengthSeconds' => 'Int32',
+          'viewCountText' => 'String',
+        ),
+      )
+    ]);
+    return $mappedData;
+  }
 
   public function mapEndscreenData($data)
   {
@@ -116,5 +236,35 @@ class Mapper
       array_push($mappedData, $mappedElement);
     }
     return $mappedData;
+  }
+
+  public function time_elapsed_string($datetime, $full = false)
+  {
+    $now = new DateTime;
+    $ago = new DateTime($datetime);
+    $diff = $now->diff($ago);
+
+    $diff->w = floor($diff->d / 7);
+    $diff->d -= $diff->w * 7;
+
+    $string = array(
+      'y' => 'year',
+      'm' => 'month',
+      'w' => 'week',
+      'd' => 'day',
+      'h' => 'hour',
+      'i' => 'minute',
+      's' => 'second',
+    );
+    foreach ($string as $k => &$v) {
+      if ($diff->$k) {
+        $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+      } else {
+        unset($string[$k]);
+      }
+    }
+
+    if (!$full) $string = array_slice($string, 0, 1);
+    return $string ? implode(', ', $string) . ' ago' : 'just now';
   }
 }
